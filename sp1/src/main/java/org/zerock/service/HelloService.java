5d8fd2427f2738220952cfcab8ca1bb4f01649b9
bitcamp27 <bitcamp27@DESKTOP-4GS1TD4>
2019-06-18 15:28:16 +0900

    테스트 환경 관련 세팅 준비

---
+++ sp1/src/main/java/org/zerock/service/HelloService.java
@@ -0,0 +1,11 @@
+package org.zerock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+	public String sayHello() {
+		return "¾È³ç ¼¼»ó¾Æ";
+	}
+}
